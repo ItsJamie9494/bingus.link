5206e29885602bd4e83a68631c44adf0c059f252
Trevor Thalacker <hello@trevorthalacker.com>
2021-04-17 15:28:02 -0700

    :bug: Make database actually save URL details

--- src/models/UrlModel.ts
+++ src/models/UrlModel.ts
@@ -17,8 +17,8 @@ import mongoose from 'mongoose'
 
 const URLSchema = new mongoose.Schema({
     urlCode: String,
-    longUrl: String,
-    shortUrl: String,
+    longURL: String,
+    shortURL: String,
     date: {
         type: String,
         default: Date.now
