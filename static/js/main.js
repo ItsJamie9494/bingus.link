d9392c247265ff9ba41dbe627ff6c05692be3c7f
Trevor Thalacker <hello@trevorthalacker.com>
2021-04-17 18:50:51 -0700

    :bug: Pressing "enter" submits form

--- static/js/main.js
+++ static/js/main.js
@@ -13,6 +13,12 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+document.getElementById('bl-f').addEventListener("submit", (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+    window.shortenURL()
+}, true)
+
 window.shortenURL = () => {
     const formDetails = {
         longURL: document.getElementById("bl-lu").value
@@ -34,21 +40,24 @@ window.shortenURL = () => {
     }).then(async res => {
         if(res.status == 200) {
             const data = await res.json();
-            document.getElementById("bl-btn").onclick = () => window.resetPage();
-            document.getElementById("bl-btn").innerHTML = "Shorten Another URL"
+            document.getElementById("bl-btn-h").onclick = () => window.resetPage();
+            document.getElementById("bl-btn-h").innerHTML = "Shorten Another URL"
+            document.getElementById("bl-btn-h").style.display = 'flex'
             document.getElementById("bl-t").innerHTML = "URL Shortened!"
             document.getElementById("bl-d").innerHTML = `${data.url}`
             document.getElementById('bl-f').style.display = 'none'
         } else {
-            document.getElementById("bl-btn").onclick = () => window.resetPage();
-            document.getElementById("bl-btn").innerHTML = "Try Again"
+            document.getElementById("bl-btn-h").onclick = () => window.resetPage();
+            document.getElementById("bl-btn-h").innerHTML = "Try Again"
+            document.getElementById("bl-btn-h").style.display = 'flex'
             document.getElementById("bl-t").innerHTML = "Something went wrong"
             document.getElementById("bl-d").innerHTML = `Please try again later`
             document.getElementById('bl-f').style.display = 'none'
         }
     }).catch(res => {
-        document.getElementById("bl-btn").onclick = () => window.resetPage();
-        document.getElementById("bl-btn").innerHTML = "Try Again"
+        document.getElementById("bl-btn-h").onclick = () => window.resetPage();
+        document.getElementById("bl-btn-h").innerHTML = "Try Again"
+        document.getElementById("bl-btn-h").style.display = 'flex'
         document.getElementById("bl-t").innerHTML = "Something went wrong"
         document.getElementById("bl-d").innerHTML = `Please try again later`
         document.getElementById('bl-f').style.display = 'none'
