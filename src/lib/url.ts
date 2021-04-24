0a8d95977a05fced1cf37d3bbd4a320494b455ab
Trevor Thalacker <hello@trevorthalacker.com>
2021-04-24 12:22:16 -0700

    :bug: Make sure protocol is HTTP or HTTPS

--- src/lib/url.ts
+++ src/lib/url.ts
@@ -1,6 +1,6 @@
 export const verifyProtocol = (url: string) => {
     // Verify that the URL has some form of a protocol, preferrably HTTP/HTTPS
-    if (url.search(/^:\/\//) == -1) {
+    if (url.search(^http[s]?:\/\/) == -1) {
         url = 'http://' + url
     }  
     return url;
