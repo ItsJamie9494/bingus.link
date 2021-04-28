cd4bdf0fd5f7d5717d69e311a23aec94a068f2e2
Trevor Thalacker <hello@trevorthalacker.com>
2021-04-27 20:58:46 -0700

    :sparkles: Custom Embeds

--- src/routes/redirect.ts
+++ src/routes/redirect.ts
@@ -14,6 +14,7 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import express from 'express'
+import { decrypt } from '../lib/crypto'
 
 import Url from '../models/UrlModel'
 
@@ -34,15 +35,28 @@ router.get('/:code', async (req: express.Request, res: express.Response ) => {
         if (url) {
             url.hitCount = (url.hitCount || 0) + 1;
             url.save()
+
+            // Decrypt Embed Info
+            let embedTitle = `This is a URL shortened with ${process.env.instanceName}`
+            let embedDescription = `Create your own at ${process.env.baseURL}`
+            let embedImage = `${process.env.baseURL}/images/bingus.png`
+
+            if (url.embedInfo) {
+                let embedInfo = JSON.parse(url.embedInfo)
+                embedTitle = decrypt(embedInfo.title)
+                embedDescription = decrypt(embedInfo.description)
+                embedImage = decrypt(embedInfo.image)
+            }
+            
             return res.render('shortURL', { 
                 url: `${process.env.baseURL}/source/${encodedURLCode}`,
                 baseUrl: process.env.baseURL,
                 title: `This is a URL shortened with ${process.env.instanceName}`,
             
                 // Custom Embed
-                embedTitle: `This is a URL shortened with ${process.env.instanceName}`,
-                embedDescription: `Create your own at ${process.env.baseURL}`,
-                embedImage: `${process.env.baseURL}/images/bingus.png`
+                embedTitle: embedTitle,
+                embedDescription: embedDescription,
+                embedImage: embedImage
                 })
         } else {
             return res.status(404).render('404', { title: '404', message: `No shortened URL was found for "${encodedURLCode}"`, baseUrl: process.env.baseURL, btnMessage: 'Create It!' })
