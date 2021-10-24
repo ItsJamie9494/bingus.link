1a750b94ce9040328bfa8dafa2f8fc88406f812e
Jamie Thalacker <hello@jamiethalacker.dev>
2021-10-23 19:52:12 -0700

    :loud_sound: update the server log shit

--- src/server.ts
+++ src/server.ts
@@ -57,6 +57,13 @@ app.get('/', (req: express.Request, res: express.Response) => {
         baseUrl: env.instance.base_url,
     })
 })
+app.get('/manage/login', (req: express.Request, res: express.Response) => {
+    res.setHeader('Permissions-Policy', 'interest-cohort=()')
+    res.render('admin/login', {
+        title: env.instance.name,
+        baseUrl: env.instance.base_url,
+    })
+})
 app.get('/license', (req: express.Request, res: express.Response) => {
     res.setHeader('Permissions-Policy', 'interest-cohort=()')
     res.render('static/license', {
@@ -103,5 +110,10 @@ app.use((req: express.Request, res: express.Response) => {
 })
 
 app.listen(port, () => {
-    console.log(`🚀 Server Started, listening on port ${port}`)
+    console.log(`
+🚀 Server Started, listening on port ${port}
+
+➡️  http://localhost:${port}
+➡️  http://127.0.0.1:${port}
+`)
 })
